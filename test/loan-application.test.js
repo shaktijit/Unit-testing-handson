9b3d5c4e5dff9a9c593449b700006cbec66ac815
shaktijit` <shaktijitpatra@gmail.com`>
2022-02-07 16:57:18 +0530

    Edited the loan-application.test.js

--- test/loan-application.test.js
+++ test/loan-application.test.js
@@ -4,4 +4,10 @@ import '../loan-application.js';
 
 describe('LoanApplication', () => {
   // Write test cases inside this block
+  it('should check the LoanApplication component', async () => {
+    const el = await fixture('<loan-application></loan-application>');
+    await expect(el).shadowDom.to.equal(`<div>
+    <dash-board> </dash-board>
+  </div>`);
+  });
 });
